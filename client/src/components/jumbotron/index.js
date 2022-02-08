6306bd69f358f45e908434ee9587b7d5736a1311
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2022-02-07 22:20:58 -0600

    form

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -3,15 +3,15 @@ import { Search } from "../Search";
 const image = "/img/1.png";
 const grass = '/img/back.jpg'
 
-const Bgimg1 = styled("div")(({ theme }) => ({
-  backgroundImage: `url(${image})`,
-  minHeight: "300px",
-  backgroundAttachment: "fixed",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  opacity: "0.65",
-}));
+// const Bgimg1 = styled("div")(({ theme }) => ({
+//   backgroundImage: `url(${image})`,
+//   minHeight: "300px",
+//   backgroundAttachment: "fixed",
+//   backgroundPosition: "center",
+//   backgroundRepeat: "no-repeat",
+//   backgroundSize: "cover",
+//   opacity: "0.65",
+// }));
 
 export const Jumbo = (props) => {
   // const x='../components/super'
